9bf48f341ac8941868e5434fc72168fb7c36eed9
cray@unix-ag.uni-kl.de <cray@unix-ag.uni-kl.de@d381f5c7-225b-5ad2-3ac6-3d3420b06a23>
2010-08-29 15:47:20 +0000

    More images for the tutorial pages.

--- trunk/tutorial/images/convert_script.bat
+++ trunk/tutorial/images/convert_script.bat
@@ -6,3 +6,10 @@ j=${i//\.png/};
 convert -scale 50% $i ${j}_small.png;
 done
 mv *_small.png smaller
+
+for i in *.jpg;
+do
+j=${i//\.jpg/};
+convert -scale 50% $i ${j}_small.jpg;
+done
+mv *_small.jpg smaller
